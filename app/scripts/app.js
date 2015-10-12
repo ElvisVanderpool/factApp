51f066ec854943951fe5991f714e2576d5ea316c
unknown <elvisvanderpool@gmail.com>
2015-10-12 18:09:22 -0400

    Inizialited the app  'factApp' and added in 'index.html'

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 (function(){
     'use strict';
-    // Main body of the angular application
-    // only declaration of main module and
-    // his dependencies
+
+   angular.module('factApp', []);
+  
 })();
